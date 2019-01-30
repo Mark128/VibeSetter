1dc6a8d1f703ab4009419b79b9163edbf9a11a1c
Mark Antoniadis <Mark.Antoniadis@fdmgroup.com>
2019-01-29 21:27:43 -0500

    First Commit - Shell Created

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
+import { MapPage } from '../map/map';
+import { ListPage } from '../list/list';
+import { ProfilePage } from '../profile/profile';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = MapPage;
+  tab2Root = ListPage;
+  tab3Root = ProfilePage;
 
   constructor() {
 
